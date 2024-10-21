5bc11e1c104640ea34f45e0f5ac335eb46a628b1
morteza <kashani.morteza@gmail.com>
2024-10-21 13:51:02 +0100

    some change

--- api.py
+++ api.py
@@ -1,19 +1,28 @@
 # webapi/api.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import route_a
 
-#-------------------------- [app]
+#--------------------------------------------------------------------------------- [App]
 api_key = "fd051ac1-e342-4631-968d-db3f19b575e7"
 app = FastAPI(root_path=f"/{api_key}")
 
-#-------------------------- [Route]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       # Allow all origins
+    allow_credentials=True,    # Allow credentials (if needed)
+    allow_methods=["*"],       # Allow all HTTP methods
+    allow_headers=["*"],       # Allow all headers
+)
+
+#--------------------------------------------------------------------------------- [Route]
 app.include_router(route_a.api_router_a, prefix="/model_a", tags=["model_a"])
 
-#-------------------------- [Root]
+#--------------------------------------------------------------------------------- [Root]
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the API"}
 
-#-------------------------- [Test]
+#--------------------------------------------------------------------------------- [Test]
 #route_a.test()
